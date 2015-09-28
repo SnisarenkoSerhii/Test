27ab41ca56a7314c00ca311cd4c00ec4ae0efccd
Sergey <sclef4@mail.ru>
2015-09-28 21:35:37 +0300

    q

--- src/testpackage/package1/HelloWorld.java
+++ src/testpackage/package1/HelloWorld.java
@@ -1,9 +1,11 @@
+package testpackage.package1;
+
 /**
  * Created by Sergey on 28.09.2015.
  */
 public class HelloWorld {
     public static void main(String args[]){
-        for (int i = 0;  i <11; i++) {
+        for (int i = 1;  i <11; i++) {
             System.out.println("Hello!"+ i);
         }
     }
