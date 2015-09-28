4dfad126dcfef211a82429fe2b7d229d676baf88
Sergey <sclef4@mail.ru>
2015-09-28 21:23:55 +0300

    q

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,8 @@
  */
 public class HelloWorld {
     public static void main(String args[]){
-        System.out.println("Hello!");
-        System.out.println("Hello2");
+        for (int i = 0;  i <11; i++) {
+            System.out.println("Hello!"+ i);
+        }
     }
 }
